b42da46411fc9b042580fbcf72bbb99ea3611c20
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-06-03 07:25:03 +0200

    batch all fields public and add proceed_input field

--- src/batch.rs
+++ src/batch.rs
@@ -5,16 +5,20 @@ use serde_derive::{Serialize, Deserialize};
 pub struct Batch {
     /// command for the package manager
     /// e.g. 'pacman -S', 'emerge -va', 'npm i g'
-    command: String,
+    pub command: String,
+
+    /// optinally string to put into stdin since some package managers expect a confirmation
+    /// usally 'Y\n' or 'y\n'
+    pub proceed_input: Option<String>,
 
     /// all items to be installed via the given package manager
-    items: Vec<String>,
+    pub items: Vec<String>,
 
     /// should next batch, if there is one, be done if this one fails
     /// true - move on to the next one
     /// false - prompt user whether pacq should continue
-    break_the_chain: bool,
+    pub break_the_chain: bool,
 
     /// should all items be installed at once or one by one
-    one_by_one: bool
+    pub one_by_one: bool
 }
