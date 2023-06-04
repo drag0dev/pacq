e78be0bb69bb255a2f52ac9baa19f67785b2f80c
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-06-04 18:57:44 +0200

    impl executing batches

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::env::args;
+use std::{
+    env::args, fs::OpenOptions
+};
 
 pub mod batch;
 pub mod config;
 pub mod utils;
 
-use utils::{load_config, load_batch};
+use utils::{
+    load_config, load_batch, execute_batch
+};
 
 // TODO: cli offers templates
 // TODO: all output to stdout also to the log file
@@ -23,6 +27,19 @@ fn main() {
     let folder_path = &args[1];
     let log_path = &args[2];
 
+    let log_file = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(log_path);
+
+    if log_file.is_err() {
+        let err = log_file.err().unwrap();
+        println!("error: opening log file: {}", err);
+        return;
+    }
+    let log_file = log_file.unwrap();
+
     let config = load_config(folder_path);
     if config.is_err() {
         let err = config.err().unwrap();
@@ -34,7 +51,6 @@ fn main() {
 
     for batch_name in config.batches {
         println!("Parsing \"{}\" batch", batch_name);
-
         let loaded_batch = load_batch(&folder_path, &batch_name);
         if loaded_batch.is_err() {
             let err = loaded_batch.err().unwrap();
@@ -43,7 +59,18 @@ fn main() {
         }
         let loaded_batch = loaded_batch.unwrap();
 
-        println!("Finished \"{}\" batch", batch_name);
-    }
+        println!("Running \"{}\" batch", batch_name);
+        let res = execute_batch(&loaded_batch, &log_file);
+        if res.is_err() {
+            let err = res.err().unwrap();
+            println!("error: {}\n\t{}", err, err.root_cause());
+            if loaded_batch.break_the_chain {
+                println!("Breaking the chain!");
+                break;
+            }
+            println!("Finished \"{}\" batch", batch_name);
+        }
 
+        println!("Finished \"{}\" batch successfully", batch_name);
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
-use std::fs::read_to_string;
+use std::{
+    fs::{read_to_string, File},
+    process::Command,
+    io::Write
+};
 use serde_json::from_str;
-use anyhow::{Result, Context};
+use anyhow::{Result, Context, anyhow};
 
 use crate::{Config, Batch};
 
@@ -24,4 +28,62 @@ pub fn load_batch(folder_path: &String, batch_name: &String) -> Result<Batch> {
         .context("parsing batch file contents")?;
 
     Ok(batch)
+}
+
+pub fn execute_batch(batch: &Batch, log_file: &File) -> Result<()> {
+    if batch.one_by_one {
+        for i in 0..batch.items.len() {
+            let res = execute_command(batch, &log_file, &batch.items[i..i])?;
+            if !res && batch.break_the_chain {
+                return Err(anyhow!("process exitted with non zero exit code"));
+            }
+        }
+    } else {
+        let res = execute_command(batch, &log_file, &batch.items[..])?;
+        if !res {
+            return Err(anyhow!("process exitted with non zero exit code"));
+        }
+    }
+
+    Ok(())
+}
+
+fn execute_command(batch: &Batch, log_file: &File, items: &[String])
+    -> Result<bool>
+{
+    let mut command = Command::new(&batch.command);
+    command.stdout(log_file
+            .try_clone()
+            .context("cloning file handler for stdout")?);
+    command.stderr(log_file
+            .try_clone()
+            .context("cloning file handler for stderr")?);
+
+    for arg in batch.args.iter() {
+        command.arg(arg);
+    }
+
+    for item in items {
+        command.arg(item);
+    }
+
+    let mut command = command
+        .spawn()
+        .context("spawning a command")?;
+
+    let mut process_stdin;
+    if let Some(input) = &batch.proceed_input {
+        if let Some(stdin) = command.stdin.take() {
+            process_stdin = stdin;
+        } else {
+            return Err(anyhow!("stdin missing from the process"));
+        }
+        process_stdin.write_all(input.as_bytes())
+            .context("writing input into the process stdin")?;
+    }
+
+    let process_res = command.wait()
+        .context("waiting for process to finish")?;
+
+    Ok(process_res.success())
 }
